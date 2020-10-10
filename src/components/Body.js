fbb843479f04f4357eb80b0c5ef90dadeaa58db0
nhungdt-1833 <do.thi.nhung@sun-asterisk.com>
2020-10-10 17:54:30 +0700

    1st Day Homework

---
+++ src/components/Body.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Body extends React.Component {
+  render() {
+    return (
+      <tbody>
+        <tr>
+          <td className="custom-td">
+            Body
+          </td>
+        </tr>
+      </tbody>
+    )
+  }
+}
+
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <thead>
+        <tr>
+          <th>
+            Header
+          </th>
+        </tr>
+      </thead>
+    )
+  }
+}
+
+export default Header;
